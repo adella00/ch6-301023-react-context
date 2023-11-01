0bea3bd745a55acb032d23006b457bf87b0d91cb
Adella Desinta <adelladesinta88@gmail.com>
2023-11-01 23:16:50 +0800

    refactor code add dan update item menggunakan react reducer

--- src/components/CartModal.jsx
+++ src/components/CartModal.jsx
@@ -1,4 +1,4 @@
-import { forwardRef, useContext, useImperativeHandle, useRef } from "react";
+import { forwardRef, useImperativeHandle, useRef } from "react";
 import { createPortal } from "react-dom";
 import Cart from "./Cart";
 
